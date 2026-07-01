50c6d3de18b9ba77e07c8a3832b42610c3b7d669
Snehil Hota <snehilhota13032006@gmail.com>
2026-07-01 21:12:51 +0530

    backend: CRUD

---
+++ backend/src/main/java/com/github/snehilhota/emp_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.github.snehilhota.emp_backend.mapper;
+
+import com.github.snehilhota.emp_backend.dto.EmployeeDto;
+import com.github.snehilhota.emp_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ backend/src/main/java/com/github/snehilhota/emp_backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,62 @@
+package com.github.snehilhota.emp_backend.service.impl;
+
+import com.github.snehilhota.emp_backend.dto.EmployeeDto;
+import com.github.snehilhota.emp_backend.entity.Employee;
+import com.github.snehilhota.emp_backend.exception.ResourceNotFoundException;
+import com.github.snehilhota.emp_backend.mapper.EmployeeMapper;
+import com.github.snehilhota.emp_backend.respository.EmployeeRepository;
+import com.github.snehilhota.emp_backend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee does not exists with the given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee does not exists with the given id: " + employeeId)
+        );
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee does not exists with the given id: " + employeeId)
+        );
+        employeeRepository.deleteById(employeeId);
+    }
+}
